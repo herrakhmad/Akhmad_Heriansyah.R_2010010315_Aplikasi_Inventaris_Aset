b992e887b774d4eaa5978426c7003ab89d1ad3d5
Akhmad Heriansyah. R <116803911+herrakhmad@users.noreply.github.com>
2023-01-14 15:36:50 +0800

    Add files via upload

---
+++ TugasAkhirPBO2_2010010315/src/inventaris_aset_barang/TugasAkhirPBO2_2010010315.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package inventaris_aset_barang;
+
+/**
+ *
+ * @author Herr
+ */
+public class TugasAkhirPBO2_2010010315 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
